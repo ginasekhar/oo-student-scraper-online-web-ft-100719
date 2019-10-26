6a6019bf4f9d655920e19aa0c71fa2fdc6dfe01d
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-26 00:08:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
     # @name = student_hash[:name]
     # @location = student_hash[:name]
     # @profile_url = student_hash[:profile_url]
@@ -17,7 +17,7 @@ class Student
 
   def add_student_attributes(attributes_hash)
 
-    attributes.each {|key, value| self.send(("#{key}="), value)}
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
   end
 
   def self.all
